e0f42e55257bb86e39d826fbe76cb00ec5d19b50
KDLBLUEICE <KDLBLUEICE@users.noreply.github.com>
2025-01-26 00:31:40 -0500

    update url

--- add2/vite.config.js
+++ add2/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 
-const baseURL = '/add2/'; // Ensure this is correct
+const baseURL = '/add2/add2'; // Ensure this is correct
 
 // https://vitejs.dev/config/
 export default defineConfig({
