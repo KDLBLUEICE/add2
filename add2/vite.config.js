c5aa53ba08cae79ef8e59132dcba87f63ae06b2a
KDLBLUEICE <KDLBLUEICE@users.noreply.github.com>
2025-01-26 00:18:58 -0500

    change vite json

--- add2/vite.config.js
+++ add2/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 
+const baseURL = '/KDLBLUEICE/add2'; // Ensure this is correct
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
